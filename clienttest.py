39d4d26d6e675db4d7ae180cf2f45b0860484392
Sobhan <sobhan.fld2@gmail.com>
2024-05-09 16:27:36 +0200

    completed

---
+++ clienttest.py
@@ -0,0 +1,16 @@
+import requests
+
+# URL of the Flask app
+url = "http://127.0.0.1:5000/auth/emailotp"
+
+# Data to be sent in the request
+data = {
+    'receivers_mail': 'sobhan.fld2@gmail.com',
+    'context': 'sobhann'
+}
+
+# Sending a POST request to the Flask server
+response = requests.post(url, data=data)
+
+# Print the response from the server
+print(response.text)
--- main.py
+++ main.py
@@ -1,60 +1,23 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import email.utils
-import uuid
-
-def send_email(subject, message, sender_email, sender_password, receiver_email):
-    # Server details
-    smtp_server = 'smtp.servidor-correo.net'
-    smtp_port = 587
-    sender_email = sender_email
-    sender_password = sender_password
-    receiver_email = receiver_email
-
-    # Create message container - the correct MIME type is multipart/alternative.
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
-    msg['Message-ID'] = email.utils.make_msgid()
-    print("This is Message ID (For header) {}".format(msg['Message-ID']))
+from flask import Flask, request, jsonify
+import json
+from Tool import send_email_complete
+# import logging
+# logging.basicConfig(filename='logemail.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-    # Create the body of the message (a plain-text and an HTML version).
-    text = message
-    html = """\
-    <html>
-      <body>
-        <p>{}</p>
-      </body>
-    </html>
-    """.format(message)
+app = Flask(__name__)
+@app.route('/auth/emailotp', methods = ['POST'])
+def rout_otp():
+    # logging.info("Received request for /auth/emailotp")
+    receivers_mail = request.form.get('receivers_mail')
+    print(receivers_mail)
+    context = request.form.get('context')
 
-    # Record the MIME types of both parts - text/plain and text/html.
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+    send_email_complete(receivers_mail, context)
+    return jsonify({
+        'status': 'success'
+    })
 
-    # Attach parts into message container.
-    msg.attach(part1)
-    msg.attach(part2)
 
-    try:
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print("Email sent successfully!")
-    except smtplib.SMTPException as e:
-        print("Error: Unable to send email -", e)
-    finally:
-        server.quit()
-
-# fields we need to send the mail
 if __name__ == "__main__":
-    subject = "Test Email"
-    message = "This is a test email using sobhan laptop."
-    sender_email = "sobhan@maxsens.es"
-    sender_password = "Asd.1234567890"
-    receiver_email = "sobhan.fld2@gmail.com"  # Change to the recipient's email address
-    send_email(subject, message, sender_email, sender_password, receiver_email)
+    app.run(host='0.0.0.0', debug=True, port=5000)
